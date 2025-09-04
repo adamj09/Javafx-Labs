2478c67c75b14b5c5ccc8ad82a5039d2b237eb8d
AdamJ <adamjohnston144@gmail.com>
2025-09-04 10:54:44 -0400

    first

---
+++ src/main/java/org/example/javafx_labs/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.javafx_labs;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
