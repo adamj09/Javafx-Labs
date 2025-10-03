a21e12b052154e1b925a58baaa04debbfbe5f10a
AdamJ <adamjohnston144@gmail.com>
2025-10-03 11:22:29 -0400

    lab 5 exercises 2 and 3

--- src/main/java/javafx_labs/lab_05_01/exercise_2/LoginPage.java
+++ src/main/java/javafx_labs/lab_05_01/exercise_2/LoginPage.java
@@ -1,5 +1,69 @@
 package javafx_labs.lab_05_01.exercise_2;
 
-public class LoginPage {
-    
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class LoginPage extends Application {
+    private Label loginInfoLabel = new Label(),
+            verificationLabel = new Label();
+
+    private TextField userField = new TextField(),
+            passField = new TextField();
+
+    private Button loginButton = new Button("Login");
+
+    private String username = "Username",
+            password = "Password";
+
+    @Override
+    public void start(Stage primaryStage) {
+        loginButton.addEventHandler(ActionEvent.ACTION, _ -> {
+            loginInfoLabel
+                    .setText("Username entered: " + userField.getText() + "\nPassword entered: " + passField.getText());
+                    
+            if (userField.getText().equals(username) && passField.getText().equals(password)) {
+                verificationLabel.setText("User is verified!");
+            } else if (!passField.getText().equals(password) && userField.getText().equals(username)) {
+                verificationLabel.setText("Incorrect password!");
+            } else if (!userField.getText().equals(username)) {
+                verificationLabel.setText("User does not exist!");
+            }
+        });
+
+        primaryStage.setScene(setupScene());
+        primaryStage.setTitle("Login Page");
+        primaryStage.show();
+    }
+
+    private Scene setupScene() {
+        Label prompt = new Label("Enter your username and password:"),
+                userLabel = new Label("Username:"),
+                passLabel = new Label("Password:");
+
+        GridPane loginPane = new GridPane(10, 10);
+        loginPane.add(prompt, 1, 0);
+        loginPane.add(userLabel, 0, 1);
+        loginPane.add(userField, 1, 1);
+        loginPane.add(passLabel, 0, 2);
+        loginPane.add(passField, 1, 2);
+        loginPane.add(loginButton, 1, 3);
+        loginPane.add(loginInfoLabel, 1, 4);
+        loginPane.add(verificationLabel, 1, 5);
+        loginPane.setPadding(new Insets(10, 10, 10, 10));
+        loginPane.setAlignment(Pos.CENTER);
+
+        return new Scene(loginPane, 300, 300);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/javafx_labs/lab_05_01/exercise_3/LoginPageTextFile.java
+++ src/main/java/javafx_labs/lab_05_01/exercise_3/LoginPageTextFile.java
@@ -1,5 +1,120 @@
 package javafx_labs.lab_05_01.exercise_3;
 
-public class LoginPageTextFile {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class LoginPageTextFile extends Application {
+    private Label loginInfoLabel = new Label(),
+            verificationLabel = new Label();
+
+    private TextField userField = new TextField(),
+            passField = new TextField();
+
+    private Button loginButton = new Button("Login");
+
+    private ArrayList<String> usernames = new ArrayList<>(),
+            passwords = new ArrayList<>();
+
+    @Override
+    public void start(Stage primaryStage) {
+        loginButton.addEventHandler(ActionEvent.ACTION, _ -> loginHandler());
+
+        primaryStage.setScene(setupScene());
+        primaryStage.setTitle("Login Page");
+        primaryStage.show();
+
+        loadLoginInfo();
+        // Login info could not be found, abort.
+        if (usernames.size() == 0 || passwords.size() == 0) {
+            System.err.println("Login info could not be found!");
+            primaryStage.close();
+        }
+    }
+
+    private void loginHandler() {
+        loginInfoLabel
+                .setText("Username entered: " + userField.getText() + "\nPassword entered: " + passField.getText());
+
+        boolean userValid = false;
+        // Check if username exists and which username was given.
+        int userIndex = 0;
+        for (int i = 0; i < usernames.size(); i++) {
+            if (usernames.get(i).equals(userField.getText())) {
+                userValid = true;
+                userIndex = i;
+                break;
+            }
+        }
+
+        if (userValid) { // If the username does exist, check whether its associated password is
+                         // correctly given.
+            if (passwords.get(userIndex).equals(passField.getText())) {
+                verificationLabel.setText("User is verified!");
+            } else if (!passwords.get(userIndex).equals(passField.getText())) {
+                verificationLabel.setText("Incorrect password!");
+            }
+        } else { // If the username does exist, state so.
+            verificationLabel.setText("User does not exist!");
+        }
+    }
+
+    private void loadLoginInfo() {
+        File file = new File("src/main/resources/users.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            // Clear arrays so that calling this method multiple times doesn't duplicate
+            // info.
+            usernames.clear();
+            passwords.clear();
+
+            while (scanner.hasNextLine()) {
+                usernames.add(scanner.next());
+                passwords.add(scanner.next());
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage());
+        } catch (NoSuchElementException ex) {
+            // Clear arrays since the info wasn't properly formatted.
+            usernames.clear();
+            passwords.clear();
+        }
+    }
+
+    private Scene setupScene() {
+        Label prompt = new Label("Enter your username and password:"),
+                userLabel = new Label("Username:"),
+                passLabel = new Label("Password:");
+
+        GridPane loginPane = new GridPane(10, 10);
+        loginPane.add(prompt, 1, 0);
+        loginPane.add(userLabel, 0, 1);
+        loginPane.add(userField, 1, 1);
+        loginPane.add(passLabel, 0, 2);
+        loginPane.add(passField, 1, 2);
+        loginPane.add(loginButton, 1, 3);
+        loginPane.add(loginInfoLabel, 1, 4);
+        loginPane.add(verificationLabel, 1, 5);
+        loginPane.setPadding(new Insets(10, 10, 10, 10));
+        loginPane.setAlignment(Pos.CENTER);
+
+        return new Scene(loginPane, 300, 300);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
